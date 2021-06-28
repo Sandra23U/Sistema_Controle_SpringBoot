7befaca08176efa8b3556e8ef8629b1fae720b21
Sandra de Oliveira Souza <66983974+Sandra23U@users.noreply.github.com>
2021-06-28 19:23:47 -0300

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,13 @@
+package com.dio.Chama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveApplication.class, args);
+    }
+
+}
